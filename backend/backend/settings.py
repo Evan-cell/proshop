7c271f2125657f55a3d19d455a66822fafc60436
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-28 13:16:14 +0300

    api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'commerce'
+    'commerce',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- backend/commerce/views.py
+++ backend/commerce/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from .products import products
+
 
 # Create your views here.
+
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+        '/api/products/upload/',
+        '/api/products/<id>/reviews/',
+        '/api/products/top/',
+        '/api/products/<id>/',
+        'api/products/delete/<id>/',
+        'api/products/<update>/<id>/'
+
+
+    ]
+    return JsonResponse(routes,safe=False,)
+def getProducts(request):
+    return JsonResponse(products, safe=False)
+
