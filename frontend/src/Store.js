70b7a71548b23b5b74f41f1997ec01aadd640625
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-03-03 10:10:44 +0300

    configure redux

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,7 +1,7 @@
 import {combineReducers,applyMiddleware} from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extentions'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers({})
 
@@ -9,6 +9,8 @@ const initialState = {}
 
 const middlware = [thunk]
 
-const store = configureStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlware)))
+const store = configureStore({reducer}, initialState, composeWithDevTools(applyMiddleware(...middlware)))
 
 export default store
+
+// , initialState, composeWithDevTools(applyMiddleware(...middlware))
