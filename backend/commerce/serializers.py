89b02a0009f89d507ec2cc8a1748a84a61ef73de
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-03-08 14:44:25 +0300

    refresh token for new users

--- backend/commerce/serializers.py
+++ backend/commerce/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     isAdmin = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'name']
+        fields = ['id', '_id','username', 'email', 'name','isAdmin']
 
     def get__id(self, obj):
         return obj.id   
@@ -22,6 +23,17 @@ class UserSerializer(serializers.ModelSerializer):
             name = obj.email
 
         return name   
+    
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)    
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
