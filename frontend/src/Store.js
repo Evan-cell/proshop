ab249337e244bbbc516b52c8783b8a14e99676ee
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-04-05 11:13:19 +0300

    authenticating user with api

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,15 +4,22 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import {productListReducer,productDetailsReducer} from './reducers/ProductReducers';
 import { cartReducer } from './reducers/CartReducers';
+import {userLoginReducer} from './reducers/Userreducers'
+
+
 const reducer = combineReducers({
     productList:productListReducer,
     productDetails:productDetailsReducer,
     cart:cartReducer,
+    userLogin:userLoginReducer,
 })
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
         JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+        JSON.parse(localStorage.getItem('userInfo')) : null       
 const initialState = {
-    cart: { cartItems : cartItemsFromStorage }
+    cart: { cartItems : cartItemsFromStorage },
+    userLogin: { userInfo : userInfoFromStorage}
 }
 
 const middlware = [thunk]
---
+++ frontend/src/components/screens/LoginScreen.js
@@ -0,0 +1,9 @@
+import React,{useState,useEffect} from 'react'
+import { Link } from 'react-router-dom'
+function LoginScreen() {
+  return (
+    <div>LoginScreen</div>
+  )
+}
+
+export default LoginScreen
