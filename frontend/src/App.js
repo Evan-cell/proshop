311768d015d709a440efb455b8afc62cccf5a1bf
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-06-21 11:42:45 +0300

    frontend auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './components/screens/HomeScreen';
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import ProductScreen from './components/screens/ProductScreen';
 import CartScreen from './components/screens/CartScreen';
+import LoginScreen from './components/screens/LoginScreen';
 function App() {
   return (
     <Router className="App">
@@ -14,6 +15,7 @@ function App() {
       <main className='py-5'>
         <Container>
           <Route path='/' component={HomeScreen} exact/>
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} exact/>
           <Route path='/Cart/:id?' component={CartScreen} exact/>
         </Container>
