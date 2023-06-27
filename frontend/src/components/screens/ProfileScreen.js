61b4a25fa3a4232ffe8bc19b43d6a913c66f88b3
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-06-27 11:23:40 +0300

    profilescreen

--- frontend/src/components/screens/ProfileScreen.js
+++ frontend/src/components/screens/ProfileScreen.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import {useDispatch,useSelector} from 'react-redux'
+import {getUserDetails} from '../../actions/Useractions'
+import Loader from '../Loader'
+import Message from '../Message'
+import FormContainer from '../FormContainer'
 
-function ProfileScreen() {
+function ProfileScreen({history}) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const dispatch = useDispatch()
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  const userRegister = useSelector(state => state.userRegister)
+  const { error, loading, userInfo } = userRegister
+
+  useEffect(() => {
+      if (userInfo) {
+          history.push(redirect)
+      }
+  }, [history, userInfo, redirect])
+
+  const submitHandler = (e) => {
+      e.preventDefault()
+
+      if (password != confirmPassword) {
+          setMessage('Passwords do not match')
+      } else {
+          dispatch(register(name, email, password))
+      }
+
+  }
   return (
-    <div>ProfileScreen</div>
+    <div></div>
   )
 }
 
